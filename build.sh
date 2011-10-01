bcbc13c30c99288d9a89bb9c9a1ef1bca85fc904
Honza Pokorny <me@honza.ca>
2011-10-01 10:30:12 -0300

    Add debug clause to js inlines.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 cat greekapp/nt/static/base.js greekapp/nt/static/ajax.js greekapp/nt/static/models.js greekapp/nt/static/views.js greekapp/nt/static/app.js > tmp.js 
 uglifyjs tmp.js > greekapp/nt/static/app.min.js
 cp greekapp/nt/static/app.min.js app.js
+rm tmp.js
