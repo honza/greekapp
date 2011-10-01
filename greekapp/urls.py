d7b641b555c9f631ee650bea8a24cc2bdff69f07
Honza Pokorny <me@honza.ca>
2011-10-01 11:28:22 -0300

    Prepare app for packaging.

--- greekapp/urls.py
+++ greekapp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'nt.views.index', name='index'),
+    url(r'^$', 'greekapp.views.index', name='index'),
     url(r'^book/(?P<book>[a-zA-Z0-9]+)/(?P<chapter>[0-9]+)/(?P<verse>[0-9]+)/$',
-        'nt.views.verse', name='verse'),
+        'greekapp.views.verse', name='verse'),
 )
